e108a43b6730aa954d09cb5af2d264672dcc79de
a.loginov <a.loginov@sakh.com>
2020-07-05 15:57:53 +1100

    Converted to Spring project

---
+++ src/main/java/linkchecker/LinkCheckerApplication.java
@@ -0,0 +1,13 @@
+package linkchecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkCheckerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/linkchecker/LinkCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package linkchecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
